9939f8bf6ffe1c54cc17f0acf6ad85b0bcf412e0
Techno Freak <freak12techno@gmail.com>
2022-12-18 01:59:34 +0300

    chore: get rid of global vars

--- main.go
+++ main.go
@@ -4,29 +4,32 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	ConfigPath string
-)
-
-var rootCmd = &cobra.Command{
-	Use:  "grafana-interacter",
-	Long: "A Telegram bot.",
-	Run:  Execute,
-}
-
-func Execute(cmd *cobra.Command, args []string) {
-	if ConfigPath == "" {
+func Execute(configPath string) {
+	if configPath == "" {
 		GetDefaultLogger().Fatal().Msg("Cannot start without config")
 	}
 
-	config := LoadConfig(ConfigPath)
+	config := LoadConfig(configPath)
 	app := NewApp(config)
 
 	app.Start()
 }
 
 func main() {
-	rootCmd.PersistentFlags().StringVar(&ConfigPath, "config", "", "Config file path")
+	var configPath string
+
+	var rootCmd = &cobra.Command{
+		Use:  "grafana-interacter",
+		Long: "A Telegram bot.",
+		Run: func(cmd *cobra.Command, args []string) {
+			Execute(configPath)
+		},
+	}
+
+	rootCmd.PersistentFlags().StringVar(&configPath, "config", "", "Config file path")
+	if err := rootCmd.MarkPersistentFlagRequired("config"); err != nil {
+		GetDefaultLogger().Fatal().Err(err).Msg("Could not set flag as required")
+	}
 
 	if err := rootCmd.Execute(); err != nil {
 		GetDefaultLogger().Fatal().Err(err).Msg("Could not start application")
