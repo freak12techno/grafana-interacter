b1941babb07dcee37e4d26e51f035037d9cf5e5b
Sergey <83376337+freak12techno@users.noreply.github.com>
2023-07-09 10:17:07 +0300

    feat: add /start command (#19)
    * feat: add /start command
    
    * chore: fixed linting

---
+++ pkg/app/help.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"fmt"
+	"main/pkg/types/render"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+func (a *App) HandleHelp(c tele.Context) error {
+	a.Logger.Info().
+		Str("sender", c.Sender().Username).
+		Str("text", c.Text()).
+		Msg("Got help query")
+
+	template, err := a.TemplateManager.Render("help", render.RenderStruct{
+		Grafana:      a.Grafana,
+		Alertmanager: a.Alertmanager,
+		Data:         nil,
+	})
+	if err != nil {
+		a.Logger.Error().Err(err).Msg("Error rendering help template")
+		return c.Reply(fmt.Sprintf("Error rendering template: %s", err))
+	}
+
+	return a.BotReply(c, template)
+}
